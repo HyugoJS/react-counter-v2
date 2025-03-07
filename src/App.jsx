2e392f2f1193c95acee74114a21aaaf737630a70
Hugo Martins <hugomartins@air-de-hugo.home>
2025-03-07 17:38:35 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,33 @@ import Counter from "./components/Counter";
 
 function App() {
   // console.log("test");
-  const [counterTab, setCounterTab] = useState([]);
-
+  const [counters, setCounters] = useState([0, 5, 8]);
+  const addCounter = () => {
+    setCounters([...counters, 0]); // Chaque compteur démarre à 0
+  };
   return (
     <>
       <h1>Counter</h1>
       <div>
-        {counterTab.map((elem, index) => {
-          return <div key={index}>{elem}</div>;
+        {counters.map((elem, index) => {
+          const newCounter = [...counters];
+          console.log(elem);
+          // newCounter.push(1);
+          return newCounter;
+          // setCounters(newCounter);
         })}
       </div>
       <button
         onClick={() => {
-          const newTab = [...counterTab];
-          newTab.push(<Counter state={counterTab[0]} />);
-          setCounterTab(newTab);
+          {
+            addCounter;
+          }
         }}
       >
         Add counter
       </button>
       <br />
+
       {/* <Counter state={counter} setState={setCounter} /> */}
     </>
   );
